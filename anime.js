fe0ca0692be29856561f89067384708f0dd37960
NaRaYeN <36738256+Narayen@users.noreply.github.com>
2021-01-03 00:42:50 +0530

    first test2

---
+++ anime.js
@@ -0,0 +1,19 @@
+var PORT = process.env.PORT || 2232;
+var express = require('express');
+var app = express();
+
+var http = require('http');
+var server = http.Server(app);
+
+var path = require('path');
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(express.static(path.join(__dirname ,'public')));
+
+app.get('/', (req, res) => res.render('pages/html/index.html'));
+
+server.listen(PORT, function() {
+  console.log('server started');
+});
